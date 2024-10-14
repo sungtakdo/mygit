66b764c47db078226f95fded8596c98b84c4bfff
dob <164461062+sungtakdo@users.noreply.github.com>
2024-10-14 19:11:16 +0900

    Delete 웹 프로젝트/dao directory

--- 웹 프로젝트/dao/BoardDAO.java
+++
@@ -1,8 +0,0 @@
-package kopo.aisw.basic_mvc.board.dao;
-
-import org.springframework.dao.DataAccessException;
-import java.util.List;
-public interface BoardDAO {
-    public List selectAllMemberList() throws DataAccessException;
-}
-
