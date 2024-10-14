dca364ba184894b150091dcfaf9d110b76d740af
dob <164461062+sungtakdo@users.noreply.github.com>
2024-10-14 19:11:23 +0900

    Delete 웹 프로젝트/service directory

--- 웹 프로젝트/service/BoardService.java
+++
@@ -1,7 +0,0 @@
-package kopo.aisw.basic_mvc.board.service;
-
-import org.springframework.dao.DataAccessException;
-import java.util.List;
-public interface BoardService {
-    public List listArticles() throws DataAccessException;
-}
